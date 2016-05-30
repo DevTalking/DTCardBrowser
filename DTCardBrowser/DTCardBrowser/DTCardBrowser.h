7cd503952d694dff7416ac75b7e0b2bfce12ec82
DevTalking <jace.fu@icloud.com>
2016-05-30 22:23:53 +0800

    Init framework files.

---
+++ DTCardBrowser/DTCardBrowser/DTCardBrowser.h
@@ -0,0 +1,19 @@
+//
+//  DTCardBrowser.h
+//  DTCardBrowser
+//
+//  Created by JaceFu on 16/5/30.
+//  Copyright © 2016年 DevTalking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTCardBrowser.
+FOUNDATION_EXPORT double DTCardBrowserVersionNumber;
+
+//! Project version string for DTCardBrowser.
+FOUNDATION_EXPORT const unsigned char DTCardBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTCardBrowser/PublicHeader.h>
+
+
